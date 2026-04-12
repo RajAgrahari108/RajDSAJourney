5435e9b5fa51d38e50012582047da7136a1021b2
Raj Agrahari <raj.agr2002@gmail.com>
2026-04-12 20:59:17 +0530

    loop practice basic

---
+++ Loop/Day 1/loopone.java
@@ -0,0 +1,8 @@
+
+public class loopone {
+    public static void main(String[] args) {
+        for(int i=0; i<=5; i++){
+            System.out.println("Value of i is : " + i);
+        }
+    }
+}
