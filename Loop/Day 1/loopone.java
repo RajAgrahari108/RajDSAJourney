ea38b88f4a837d68b80db39c1e45abb32e0bc9c0
Raj Agrahari <raj.agr2002@gmail.com>
2026-04-17 08:46:08 +0530

    Pattern Practice for making my dsa logic strong

--- Loop/Day 1/loopone.java
+++ Loop/Day 1/loopone.java
@@ -2,7 +2,7 @@
 public class loopone {
     public static void main(String[] args) {
         for(int i=0; i<=5; i++){
-            System.out.println("Value of i is : " + i)
-        
+            System.out.println("Value of i is : " + i);
+        }
     }
 }
---
+++ PAttern/two.java
@@ -0,0 +1,11 @@
+ public class two{
+    public static void main(String main[]){
+        int n= 5;
+        for(int row=1; row<=n; row++){
+            for(int col=1; col<row; col++ ){
+                System.out.print(" *");
+            }
+            System.out.println( );
+        }
+    }
+ }
