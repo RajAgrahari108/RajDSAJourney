c25e46ebc31c417d388b9d42b991fb8a5ece4015
Raj Agrahari <raj.agr2002@gmail.com>
2026-07-09 21:36:46 +0530

    Submiting all the code which i practice

--- Extra code/Home.java
+++
@@ -1,7 +0,0 @@
-public class Home {
-    public static void main(String[] args) {
-        int a = 2, b=3;
-        int sum  = a+b;
-        System.out.println("the value of a and b is : " + sum);
-    }
-}
