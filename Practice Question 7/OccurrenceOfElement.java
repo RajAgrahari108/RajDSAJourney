1a0f42b656841ee00d5f8b5c66d13f23adfa063f
Raj Agrahari <raj.agr2002@gmail.com>
2026-08-03 22:47:04 +0530

    code submitig or updating the code

---
+++ Practice Question 7/OccurrenceOfElement.java
@@ -0,0 +1,20 @@
+public class OccurrenceOfElement {
+    public static void main(String[] args) {
+        int arr[] = {4, 2, 6, 2, 9, 2};
+        int target = 2;
+
+        noOfOccurrence(arr, target);
+    }
+
+    public static void noOfOccurrence(int arr[], int target) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                count++;
+            }
+        }
+
+        System.out.println("Number of occurrences: " + count);
+    }
+}
--- Practice Question 7/TrappedRainWater.java
+++ Practice Question 7/TrappedRainWater.java
@@ -1,5 +1,5 @@
 public class TrappedRainWater {
-    public static void main(String[] args) 
+    public static void main(String[] args) {
         int arr[]={};
 
     }
